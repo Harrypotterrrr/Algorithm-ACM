450e42b59897a47216ff5a2d6ed8fce1c32308e1
Harrypotterrrr <jiahaolin19971119@126.com>
2019-02-04 02:50:07 +0800

    :izakaya_lantern: add one code 2/4/2019

---
+++ Array/LC 0238 - bilateralScaning.cpp
@@ -0,0 +1,89 @@
+/*
+238. Product of Array Except Self
+
+Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+Example:
+
+Input:  [1,2,3,4]
+Output: [24,12,8,6]
+Note: Please solve it without division and in O(n).
+
+*/
+
+#include "../include/include.h"
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int>rtn(nums.size());
+        int mul=1;
+        for(auto i=0; i<nums.size()-1 ; i++){
+            mul *= nums[i];
+            rtn[i] = mul;
+        }
+        mul = 1;
+        for(auto i=nums.size()-1 ; i > 0 ; i--){
+            rtn[i] = rtn[i-1] * mul;
+            mul *= nums[i];
+        }
+        rtn[0] = mul;
+        return rtn;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+        
+        vector<int> ret = Solution().productExceptSelf(nums);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
