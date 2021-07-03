85833ffca270abb8ed995f7172a5020404787cdd
harrypotterrrr <jiahaolin19971119@126.com>
2021-07-03 16:51:30 +0800

    add 2 med LinkedList

--- Linker/LC 0160 - cycle.cpp
+++ Linker/LC 0160 - cycle.cpp
@@ -2,14 +2,40 @@
 160. Intersection of Two Linked Lists
 */
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+// Solution 1: compare the length
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        int num1 = 0, num2 = 0;
+        ListNode *p = headA, *q = headB;
+        while(p){
+            p = p->next;
+            num1 ++;
+        }
+        while(q){
+            q = q->next;
+            num2 ++;
+        }
+        if(num1 > num2)
+            while(num1-- > num2) 
+                headA = headA->next;
+        else if(num1 < num2)
+            while(num2 -- > num1)
+                headB = headB->next;
+        
+        while(headA && headB){
+            if(headA == headB)
+                return headA;
+            headA = headA->next;
+            headB = headB->next;
+        }
+        return NULL;
+    }
+};
+
+// Solution 2: cycle
+
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
