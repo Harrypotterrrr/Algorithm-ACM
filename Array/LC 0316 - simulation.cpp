7303460f708ea8b3b4d7ecebfbb472ef14605faa
Harrypotterrrr <jiahaolin19971119@126.com>
2021-09-12 14:54:58 +0800

    add 1 med 1 easy

--- Array/LC 0316 - simulation.cpp
+++ Array/LC 0316 - simulation.cpp
@@ -1,26 +1,25 @@
 /*
-88. Merge Sorted Array
+316. Remove Duplicate Letters
 */
 
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> nums1_bak;
-        for(int i=0 ;i<m;i++)
-            nums1_bak.push_back(nums1[i]);
+    string removeDuplicateLetters(string s) {
+        string ans;
+        vector<bool>visited(26, false);
+        vector<int>ctr(26, 0);
+        for(auto &i: s) ctr[i-'a'] ++;
+        for(auto &i: s){
+            ctr[i-'a'] --;
+            if(visited[i-'a']) continue;
+            while(ans.size() && i < ans.back() && ctr[ans.back()-'a']){
+                visited[ans.back()-'a'] = false;
+                ans.pop_back();
+            }
+            ans += i;
+            visited[i-'a'] = true;
+        }
         
-        int p=0, q=0, r=0;
-        while(p<nums1_bak.size() && q <nums2.size()){
-            if(nums1_bak[p] > nums2[q])
-                nums1[r++] = nums2[q++];
-            else
-                nums1[r++] = nums1_bak[p++];
-        }
-        while(p<nums1_bak.size()){
-            nums1[r++] = nums1_bak[p++];
-        }
-        while(q<nums2.size()){
-            nums1[r++] = nums2[q++];
-        }
+        return ans;
     }
 };
