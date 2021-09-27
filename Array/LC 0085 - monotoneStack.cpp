2ded0abd1e653bdf6814dcfc94d25807c86165cc
Harrypotterrrr <jiahaolin19971119@126.com>
2021-09-26 23:59:39 -0400

    update hard TODO

--- Array/LC 0085 - monotoneStack.cpp
+++ Array/LC 0085 - monotoneStack.cpp
@@ -2,7 +2,7 @@
 85. Maximal Rectangle
 */
 
-// Solution 1: TLE O(n^2)
+// Solution 1: O(mn) using monotone stack
 
 class Solution {
 public:
@@ -43,4 +43,68 @@ public:
     }
 };
 
-// Solution 2: TODO
+// Solution 2: scan line by line using time of O(mn)
+
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if(matrix.size() == 0) return 0;
+        int n = matrix.size(), m = matrix[0].size(), ans = 0;
+        vector<int> h(m, 0), l(m, 0), r(m, m-1);
+        for(int i=0 ; i<n ; i++){
+            int left_k = 0, right_k = m-1;
+            for(int j=0 ; j<m ; j++){
+                if(matrix[i][j] == '1'){
+                    h[j]++;
+                    l[j] = l[j] > left_k ? l[j] : left_k;
+                }
+                else{
+                    h[j] = 0;
+                    l[j] = 0;
+                    left_k = j+1;
+                }
+            }
+            for(int j=m-1 ; j>=0 ; j--){
+                if(matrix[i][j] == '1'){
+                    r[j] = r[j] < right_k ? r[j] : right_k;
+                    ans = ans > h[j] * (r[j] - l[j] + 1) ? ans : h[j] * (r[j] - l[j] + 1);
+                }
+                else{
+                    r[j] = m-1;
+                    right_k = j-1;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// Solution 3: calculate adjacent elemetns O(mn^2)
+
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        if(matrix.size() == 0) return 0;
+        int n = matrix.size(), m = matrix[0].size(), ans = 0;
+        vector<vector<int>>vec(n, vector<int>(m, 0));
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(matrix[i][j] == '0') continue;
+                if(j > 0) vec[i][j] = vec[i][j-1] + 1;
+                else vec[i][j] = 1;
+            }
+        }
+        
+        for(int i=0 ; i<n ; i++){
+            for(int j=0 ; j<m ; j++){
+                if(matrix[i][j] == '0') continue;
+                int width = vec[i][j];
+                for(int k=i ; k>=0 && vec[k][j] > 0 ; k--){
+                    width = width < vec[k][j] ? width : vec[k][j];
+                    ans = ans > width * (i - k + 1) ? ans : width * (i - k + 1);
+                }
+            }
+        }
+        return ans;
+    }
+};
